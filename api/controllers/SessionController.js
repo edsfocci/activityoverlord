54694bb07064246c8def067354277e41e312c821
Ed Solis <edsfocci@gmail.com>
2015-05-02 13:39:39 -0500

    Sign-in page, session controller w/ new action, working w/ sessions

---
+++ api/controllers/SessionController.js
@@ -0,0 +1,23 @@
+/**
+ * SessionController
+ *
+ * @description :: Server-side logic for managing sessions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+  'new': function(req, res) {
+
+    var oldDateObj = new Date();
+    var newDateObj = new Date(oldDateObj.getTime() + 60000);
+    req.session.cookie.expires = newDateObj;
+    req.session.authenticated = true;
+    console.log(req.session);
+    // console.log(new Date());
+
+    res.view();
+  }
+	
+};
+
